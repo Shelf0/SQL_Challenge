8c7a5ac94d1a7b581a61daf5615a8774ba78ab66
Shelf0 <shelffo4@gmail.com>
2021-09-19 22:12:58 -0400

    added code for manager lists

--- Employee_SQL/Organize.sql
+++ Employee_SQL/Organize.sql
@@ -9,3 +9,7 @@ WHERE hire_date >= '1986-01-01'
 	AND hire_date <= '1986-12-31';
 
 
+SELECT departments.dep_no, departments.dept_name, dep_manager.emp_no, employees.last_name, employees.first_name
+FROM departments
+JOIN dep_manager ON departments.dep_no = dep_manager.dep_no
+JOIN employees ON dep_manager.emp_no = employees.emp_no;
