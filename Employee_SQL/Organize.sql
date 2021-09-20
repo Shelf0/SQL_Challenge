6fbf53be485a1b056f5afd6f7c1dc751a128a300
Shelf0 <shelffo4@gmail.com>
2021-09-19 22:11:49 -0400

    Added code for 1986 hires

--- Employee_SQL/Organize.sql
+++ Employee_SQL/Organize.sql
@@ -3,5 +3,9 @@ FROM employees
 JOIN salaries on employees.emp_no = salaries.emp_no; 
 
 
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date >= '1986-01-01'
+	AND hire_date <= '1986-12-31';
 
 
